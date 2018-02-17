f4dd23611a2c0132fed04b7d22c453257cf513c8
Jigowa <jogawa@u.rochester.edu>
2018-02-16 21:36:13 -0500

    Fixed nav

--- nav.php
+++ nav.php
@@ -1,9 +1,9 @@
 <nav>
 	<ul>
-		<li><a href="sheet.html">Home</a></li>
-		<li><a href="offence.html">Offence</a></li>
-		<li><a href="defence.html">Defence</a></li>
-		<li><a href="skills.html">Skills</a></li>
-		<li><a href="inventory.html">Inventory</a></li>
+		<li><a href="sheet.php">Home</a></li>
+		<li><a href="offence.php">Offence</a></li>
+		<li><a href="defence.php">Defence</a></li>
+		<li><a href="skills.php">Skills</a></li>
+		<li><a href="inventory.php">Inventory</a></li>
 	</ul>
 </nav>

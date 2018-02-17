fff4c90749c86babccb6132846c7e2445d0510e6
Jigowa <jogawa@u.rochester.edu>
2018-02-17 11:30:48 -0500

    Combined O+D into Combat

--- combat.php
+++ combat.php
@@ -87,5 +87,29 @@
 				<label>Other</label>
 			</div>
 		</form>
+
+		<h1>Offence</h1>
+		<form>
+			<div class="offence">
+				<label>Initiative: </label>
+				<input type="text" name="init" id="init">
+			</div>
+			<div class="offence">
+				<label>Base Attack Bonus: </label>
+				<input type="text" name="bab" id="bab">
+			</div>
+
+			<h2>Weapon Attacks</h2>
+				<div class="offence weaponattack">
+					<label>Weapon: </label>
+					<input type="text" name="weapon1" id="weapon1">
+					<label>Bonus: </label>
+					<input type="text" name="bonus1" id="bonus1">
+					<label>Damage: </label>
+					<input type="text" name="damage1" id="damage1">
+					<label>Special: </label>
+					<input type="text" name="special1" id="special1">
+				</div>
+		</form>
 	</body>
 </html>

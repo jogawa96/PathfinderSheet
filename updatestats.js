0cfe3f1f9215339033774b6109c3f5e4903cbb48
Jigowa <jogawa@u.rochester.edu>
2018-02-17 17:27:44 -0500

    Sidebar updates

--- updatestats.js
+++ updatestats.js
@@ -5,21 +5,26 @@ var int = document.getElementById("intscore");
 var wis = document.getElementById("wisscore");
 var cha = document.getElementById("chascore");
 
-str.addEventListener("change", function(){update("str");});
-dex.addEventListener("change", function(){update("dex");});
-con.addEventListener("change", function(){update("con");});
-int.addEventListener("change", function(){update("int");});
-wis.addEventListener("change", function(){update("wis");});
-cha.addEventListener("change", function(){update("cha");});
+var strscore = 0;
+var dexscore = 0;
+var conscore = 0;
+var intscore = 0;
+var wisscore = 0;
+var chascore = 0;
+
+str.addEventListener("change", function(){strscore = update("str");});
+dex.addEventListener("change", function(){dexscore = update("dex");});
+con.addEventListener("change", function(){conscore = update("con");});
+int.addEventListener("change", function(){intscore = update("int");});
+wis.addEventListener("change", function(){wisscore = update("wis");});
+cha.addEventListener("change", function(){chascore = update("cha");});
 
 function update(x)
 {
-	console.log(x);
 	var scorefield = document.getElementById(x + "score");
 	var modfield = document.getElementById(x + "mod");
-	console.log(scorefield);
+	var sidefield = document.getElementById("side" + x);
 	var score = scorefield.value;
-	console.log(score);
 	var mod = Math.floor((score - 10)/2);
 	var sign = "";
 	if(mod < 0)
@@ -29,4 +34,5 @@ function update(x)
 	var modifier = sign + mod;
 	console.log(modifier);
 	modfield.setAttribute("value", modifier);
+	sidefield.setAttribute("value", score);
 }

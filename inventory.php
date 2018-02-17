a0e42a89e44f93d1b2aefd435bf3551ea671bd83
Jigowa <jogawa@u.rochester.edu>
2018-02-17 16:57:43 -0500

    Fixed inventory

--- inventory.php
+++ inventory.php
@@ -4,7 +4,7 @@
 		<meta charset="utf-8">
 		<title>Pathfinder Character Sheet</title>
 		<link rel="stylesheet" href="styles.css">
-		<link rel="stylesheet" href="PathfinderSheet/css/styles.css">
+		<link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
 	</head>
 
 	<body>

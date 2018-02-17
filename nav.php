6fe669854b9f101fd1287a198af6e69d16952928
Jigowa <jogawa@u.rochester.edu>
2018-02-17 12:30:50 -0500

    Updated CSS

--- nav.php
+++ nav.php
@@ -1,9 +1,9 @@
 <nav>
 	<ul>
-		<li><a href="sheet.php">Home</a></li>
-		<li><a href="combat.php">Combat</a></li>
-		<li><a href="skills.php">Skills</a></li>
-		<li><a href="inventory.php">Inventory</a></li>
-		<li><a href="feats.php">Feats</a></li>
+		<li class="tab"><a href="sheet.php">Home</a></li>
+		<li class="tab"><a href="combat.php">Combat</a></li>
+		<li class="tab"><a href="skills.php">Skills</a></li>
+		<li class="tab"><a href="inventory.php">Inventory</a></li>
+		<li class="tab"><a href="feats.php">Feats</a></li>
 	</ul>
 </nav>

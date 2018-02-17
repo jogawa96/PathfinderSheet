c056f484e902a560f513d5ac6056721b2ad1d918
Jigowa <jogawa@u.rochester.edu>
2018-02-17 18:51:01 -0500

    Touch + FF AC Auto

--- updatedef.js
+++ updatedef.js
@@ -5,6 +5,7 @@ var dex = document.getElementById("acdex");
 var size = document.getElementById("acsize");
 var natural = document.getElementById("acnatural");
 var deflection = document.getElementById("acdeflection");
+var dodge = document.getElementById("acdodge");
 var other = document.getElementById("acother");
 
 armour.addEventListener("change", function(){update();});
@@ -13,6 +14,7 @@ dex.addEventListener("change", function(){update();});
 size.addEventListener("change", function(){update();});
 natural.addEventListener("change", function(){update();});
 deflection.addEventListener("change", function(){update();});
+dodge.addEventListener("change", function(){update();});
 other.addEventListener("change", function(){update();});
 
 function update()
@@ -20,8 +22,8 @@ function update()
 	var acfield = document.getElementById("ac");
 	var total = 10;
 
-	var bonuses = [armour, shield, dex, size, natural, deflection, other];
-	var values = [0, 0, 0, 0, 0, 0, 0];
+	var bonuses = [armour, shield, dex, size, natural, deflection, dodge, other];
+	var values = [0, 0, 0, 0, 0, 0, 0, 0];
 
 	for (var i = bonuses.length - 1; i >= 0; i--) {
 		if(bonuses[i].value != "")
@@ -29,6 +31,9 @@ function update()
 			values[i] = bonuses[i].value;
 			total = total + parseInt(values[i]);
 		}
+		values[i] = parseInt(values[i]);
 	}
-	document.getElementById("ac").value = (total);
+	acfield.value = total;
+	var touch = document.getElementById("actouch").value = 10 + values[2] + values[3] + values[5] + values[6] + values[7];
+	var ff = document.getElementById("acflat").value = 10 + values[0] + values[1] + values[3] + values[4] + values[5] + values[7];
 }

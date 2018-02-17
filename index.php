68947cbdb917f3ca54a42ccd9a26e0a140869834
Alex Livings <alivings@fingerlakes.edu>
2018-02-17 17:50:38 -0500

    working on create character

--- index.php
+++ index.php
@@ -49,8 +49,10 @@
     <?php  if (isset($_SESSION['username'])) : ?>
     	<p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
     	<p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
-    <?php endif ?>
-    <button type="submit" class="btn" name="character_create">Create New Character</button>
+	<?php endif ?>
+	<div class="input-group"
+	<button type="submit" class="btn" name="character_create">Create New Character</button>
+</div>
 </div>
 		
 </body>

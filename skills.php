19449a76b664b7efed41831ef430e8063444f851
Jigowa <jogawa@u.rochester.edu>
2018-02-17 07:24:43 -0500

    Skills

---
+++ skills.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Pathfinder Character Sheet</title>
+	</head>
+
+	<body>
+
+		<?php include 'nav.php';?>
+
+		<!-- THE STICKY INCLUDE WILL BE THE BIT THAT STICKS TO A CORNER -->
+		<?php include 'sticky.php';?>
+
+		<h1>Offence</h1>
+		<form>
+			<div class="offence">
+				<label>Initiative: </label>
+				<input type="text" name="init" id="init">
+			</div>
+		</form>
+	</body>
+</html>

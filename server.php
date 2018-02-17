061f3628fb18dfe2fb002a41bfb600d888fff9bd
Alex Livings <alivings@fingerlakes.edu>
2018-02-17 18:49:39 -0500

    test

--- server.php
+++ server.php
@@ -82,6 +82,18 @@ if (isset($_POST['login_user'])) {
   }
 
   if (isset($_POST['create_character'])) {
+  
+    $username = $username;
+
+      $password = md5($password_1);//encrypt the password
+  
+      $query = "INSERT INTO character (username) 
+            VALUES('$username')";
+           mysqli_query($db, $query);
+        header('location: sheet.php');
+    }
+
+  if (isset($_POST['create_character'])) {
 
     $sql = "SELECT userid FROM users WHERE username = $username";
     $result = $conn->query($sql);

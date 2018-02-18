4f8fa37afdc4b74e78bb08111b15bc188edc8177
Alex Livings <alivings@fingerlakes.edu>
2018-02-18 01:13:05 -0500

    index issues

--- index.php
+++ index.php
@@ -1,29 +1,10 @@
 <?php include('server.php'); session_start(); ?>
-<html>
-    <head>
-		<title>Home Page</title>
-		<link rel="stylesheet" href="test.css">
-    <link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
-	</head>
 
-<?php 
-  session_start(); 
-
-  if (!isset($_SESSION['username'])) {
-  	$_SESSION['msg'] = "You must log in first";
-  	header('location: login.php');
-  }
-  if (isset($_GET['logout'])) {
-  	session_destroy();
-  	unset($_SESSION['username']);
-  	header("location: login.php");
-  }
-?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Home</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="test.css">
 </head>
 <body>
 

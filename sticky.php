5d2c777910458a8e5de94fc372f07c325cd5a592
Alex Livings <alivings@fingerlakes.edu>
2018-02-18 00:00:36 -0500

    added sticky styles for bold

--- sticky.php
+++ sticky.php
@@ -1,44 +1,45 @@
 <?php include('server.php'); session_start(); ?>
+<link href="style2.css" rel="stylesheet" />
 <aside>
 	<form>
 		<div class="sidestats">
 			<div class="sidephysical">
 				<div class="sidestat">
 					<label class="lab">STR</label>
-					<input type="text" name="sidestr" id="sidestr" readonly>
+					<input class="stat" type="text" name="sidestr" id="sidestr" readonly>
 				</div>
 				<div class="sidestat">
 					<label>DEX</label>
-					<input type="text" name="sidedex" id="sidedex" readonly>
+					<input class="stat" type="text" name="sidedex" id="sidedex" readonly>
 				</div>
 				<div class="sidestat">
 					<label>CON</label>
-					<input type="text" name="sidecon" id="sidecon" readonly>
+					<input class="stat" type="text" name="sidecon" id="sidecon" readonly>
 				</div>
 			</div>
 			<div class="sidemental">
 				<div class="sidestat">
 					<label>INT</label>
-					<input type="text" name="sideint" id="sideint" readonly>
+					<input class="stat" type="text" name="sideint" id="sideint" readonly>
 				</div>
 				<div class="sidestat">
 					<label>WIS</label>
-					<input type="text" name="sidewis" id="sidewis" readonly>
+					<input class="stat" type="text" name="sidewis" id="sidewis" readonly>
 				</div>
 				<div class="sidestat">
 					<label>CHA</label>
-					<input type="text" name="sidecha" id="sidecha" readonly>
+					<input class="stat" type="text" name="sidecha" id="sidecha" readonly>
 				</div>
 			</div>
 		</div>
 		<div class="sidehealth">
 			<div class="sidestat">
 				<label>HP: </label>
-				<input type="text" name="sidehp" id="sidehp" readonly>
+				<input class="stat" type="text" name="sidehp" id="sidehp" readonly>
 			</div>
 			<div class="sidestat">
 				<label>Damage: </label>
-				<input type="text" name="sidedamage" id="sidedamage">
+				<input class="stat" type="text" name="sidedamage" id="sidedamage">
 			</div>
 		</div>
 	</form>

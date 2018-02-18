1014192ef30cfedca5904d0adda40678f47afa90
Alex Livings <alivings@fingerlakes.edu>
2018-02-17 23:40:48 -0500

    debug?

--- server.php
+++ server.php
@@ -92,11 +92,11 @@ if (isset($_POST['login_user'])) {
      $dexterityraw = mysqli_real_escape_string($db, $_POST['dexterityraw']);
      $_SESSION['character'] = $charname;
     
-      /*
-      $query = "INSERT INTO character (username, char_name, charisma_raw, wisdom_raw, intel_raw, strength_raw, constitution_raw, dexterity_raw) 
+      
+      $query = "INSERT INTO `characters` (`username`, `char_name`, `charisma_raw`, `wisdom_raw`, `intel_raw`, `strength_raw`, `constitution_raw`, `dexterity_raw`) 
                   VALUES('{$_SESSION[username]}' , '$charname', '$charismaraw', '$wisdomraw', '$intelraw', '$strengthraw', '$constitutionraw', '$dexterityraw')";
-      */
-      $query = "INSERT INTO `characters` (`username`, `char_name`) VALUES ('{$_SESSION[username]}', '$charname')";
+      
+      //$query = "INSERT INTO `characters` (`username`, `char_name`) VALUES ('{$_SESSION[username]}', '$charname')";
       mysqli_query($db, $query);
       header('location: combat.php');
       }

15821a8ea3fd4fbbad63423ec04cd41d4d4a0511
Alex Livings <alivings@fingerlakes.edu>
2018-02-19 14:41:34 -0500

    fixing input for login sheet test

--- login.php
+++ login.php
@@ -18,11 +18,11 @@ session_start();
   	<?php include('errors.php'); ?>
   	<div class="input-group">
   		<label>Username</label>
-  		<input type="text" name="username" >
+  		<input class="userinfo" type="text" name="username" >
   	</div>
   	<div class="input-group">
   		<label>Password</label>
-  		<input type="password" name="password">
+  		<input class="userinfo" type="password" name="password">
   	</div>
   	<div class="input-group">
   		<button type="submit" class="btn" name="login_user">Login</button>

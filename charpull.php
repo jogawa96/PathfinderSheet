0f5523ebf65e5abdaf084a3b0bce47cd9337f681
Alex Livings <alivings@fingerlakes.edu>
2018-02-19 21:05:01 -0500

    cleaned up charpull sheet

--- charpull.php
+++ charpull.php
@@ -18,26 +18,4 @@ while(list($charnames)=$rows->fetch_row()){
   echo "<input type=\"submit\" name=\"charpick\" value=\"$charnames\"/>
   </div>";
 }
-echo "<input name=\"pickchar\" type=\"submit\" value=\"ChooseCharacter\"/></div></form>";
-
-/*
-//showing property
-echo '<table class="data-table">
-        <tr class="data-heading">';  //initialize table tag
-while ($property = mysqli_fetch_field($character_check_query)) {
-    echo '<td>' . $property->name . '</td>';  //get field name for header
-    array_push($all_property, $property->name);  //save those to array
-}
-echo '</tr>'; //end tr tag
-
-//showing all data
-while ($row = mysqli_fetch_array($result)) {
-    echo "<tr>";
-    foreach ($all_property as $item) {
-        echo '<td>' . $row[$item] . '</td>'; //get items using property value
-    }
-    echo '</tr>';
-}
-echo "</table>";
-*/
 ?>

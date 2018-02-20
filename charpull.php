feebfc644ca7e7c22ce7a7f18a992bc3a303d5d9
Alex Livings <alivings@fingerlakes.edu>
2018-02-19 19:59:08 -0500

    attempting to pull char info from database

--- charpull.php
+++ charpull.php
@@ -9,7 +9,7 @@ $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 $character_check_query = "SELECT char_name FROM characters WHERE username='{$_SESSION[username]}'";
 $all_property = array();
 
-$rows=$db->query("SELECT char_name FROM characters WHERE username='testing'");
+$rows=$db->query("SELECT char_name FROM characters WHERE username='{$_SESSION['username']}'");
 
 echo "<table border='1'>";
 echo "<tr><th>char_name</th></tr>";

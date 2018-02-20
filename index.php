21e69c55cdbfc0c799a6ed906be027d054af6110
Alex Livings <alivings@fingerlakes.edu>
2018-02-19 21:47:23 -0500

    moving char create away from index

--- index.php
+++ index.php
@@ -30,46 +30,10 @@
 	<?php endif ?>
 
 	<!-- trying to pull this user's characters from the database-->
+	<?php include charpull.php ?>
 
+	<a href="charcreate.php"><button value="Create New Character"/></a>
 
-	<form method="post" action="index.php">
-    <!-- VALIDATION ERRORS -->
-    <?php include('errors.php'); ?>
-	<h3> Base Character Design </h3>
-        <div class="input-group">	
-        <label>Character Name</label>
-        <input type="text" name="charname" value="<?php echo $charname; ?>">
-		</div>
-		<h3> Base Character Stats</h3>
-		<div class="input-group">	
-        <label>Raw DEX</label>
-        <input type="text" name="dexterityraw" value="<?php echo $dexterityraw; ?>">
-		</div>
-		<div class="input-group">	
-        <label>Raw CON</label>
-        <input type="text" name="constitutionraw" value="<?php echo $constitutionraw; ?>">
-		</div>
-		<div class="input-group">	
-        <label>Raw INT</label>
-        <input type="text" name="intelraw" value="<?php echo $intelraw; ?>">
-		</div>
-		<div class="input-group">	
-        <label>Raw WIS</label>
-        <input type="text" name="wisdomraw" value="<?php echo $wisdomraw; ?>">
-		</div>
-		<div class="input-group">	
-        <label>RAW CHA</label>
-        <input type="text" name="charismaraw" value="<?php echo $charismaraw; ?>">
-		</div>
-        <div class="input-group">	
-        <label>RAW STR</label>
-        <input type="text" name="strengthraw" value="<?php echo $strengthraw; ?>">
-		</div>
-        <div class="input-group">
-        <button type="submit" name="create" class="btn" >Create New Character</button>
-		</div>
-    </form>
-</div>
 	<footer><form method="post"><div class="input-group"><button name="logout_user" class="btn"> Logout </button> </div></form></footer>
 
 </body>

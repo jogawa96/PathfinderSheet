6ad4b3cc65d1d3264c388dfc2ccd70327f5ae316
Alex Livings <alivings@fingerlakes.edu>
2018-02-21 10:19:19 -0500

    logout automatically redirects to login page

--- logout.php
+++ logout.php
@@ -4,19 +4,6 @@
 <?php
     session_start();
     session_destroy();
-    echo '
-    <!DOCTYPE html>
-    <html>
-        <head>
-            <meta charset=\"utf-8\"/>
-            <link rel=\"stylesheet\" href=\"styles2.css\"/>
-        </head>
-        <body>
-            <div>
-                You have been logged out.
-                <button onclick=\"location.href=\'login.php\'\" type=\"button\">Return to Login Page</button>
-            </div>
-        You have been logged out
-        </body>
-    </html>';
+    header("Location: login.php");
+    exit;
 ?>

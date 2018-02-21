8ce2f6ebf923837a2b2058b19917e93a13bce834
Pathfinder <kcheung6@u.rochester.edu>
2018-02-21 19:23:42 +0000

    testing some values

--- setchar.php
+++ setchar.php
@@ -9,6 +9,9 @@ $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 $_SESSION['character'] = $_GET['charnames'];
 $get_character_info = "SELECT 'charisma_raw' FROM `characters` WHERE username='{$_SESSION['username']}' AND char_name='{$_SESSION['character']}'";
 $test = mysqli_query($db, $get_character_info);
+$row=$test->fetch_assoc();
+$charismaraw = intval($row['charisma_raw']);
+$_SESSION['charismaraw']=$charismaraw;
 
 ?>
 

b6ffc4e772299f217fbfbd8c878851560c048356
Alex Livings <alivings@fingerlakes.edu>
2018-02-21 12:51:12 -0500

    make buttons come back

--- charpull.php
+++ charpull.php
@@ -15,7 +15,7 @@ $rows=$db->query("SELECT char_name FROM characters WHERE username='{$_SESSION['u
 
 echo "<div class=\"container\"><form method=\"POST\" action=\"index.php\"><h1>Characters</h1>";
 while(list($charnames)=$rows->fetch_row()){
-  echo "<button type=\"submit\" name=\"charpick\" class=\"btn\" value=\"$charnames\"/>";
+  echo "<input type=\"submit\" name=\"charpick\" value=\"$charnames\"/>";
 }
 echo "</form></div>";
 

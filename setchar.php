f80716aa4a2aa95d681d4f06b273989aa6a3b4bf
Alex Livings <alivings@fingerlakes.edu>
2018-02-21 13:58:56 -0500

    testing

--- setchar.php
+++ setchar.php
@@ -1,7 +1,17 @@
 <?php
 include('server.php');
 session_start();
+require_once "../config.php";
+
+
+// connect to the database (need database info here) 
+$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 $_SESSION['character'] = $_GET['charnames'];
+
+$pull_character_info = "SELECT charisma_raw FROM `characters` WHERE username={$_SESSION[username]} AND char_name={$_SESSION[character]}";
+
+$_SESSION['charismaraw'] = mysqli($db, $pull_character_info);
+
 
 ?>

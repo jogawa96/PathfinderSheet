e68399deedd39f4d58e65abe2de982dd0bbe9775
Pathfinder <kcheung6@u.rochester.edu>
2018-02-22 01:49:16 +0000

    the numbers have arrived my friends

--- setchar.php
+++ setchar.php
@@ -7,11 +7,15 @@ require_once "../config.php";
 $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 $_SESSION['character'] = $_GET['charnames'];
-$get_character_info = "SELECT 'charisma_raw' FROM `characters` WHERE username='{$_SESSION['username']}' AND char_name='{$_SESSION['character']}'";
+$get_character_info = "SELECT charisma_raw, strength_raw, intel_raw, constitution_raw, wisdom_raw, dexterity_raw FROM characters WHERE username='{$_SESSION['username']}' AND char_name='{$_SESSION['character']}'";
 $test = mysqli_query($db, $get_character_info);
-$row=$test->fetch_assoc();
-$charismaraw = intval($row['charisma_raw']);
-$_SESSION['charismaraw']=$charismaraw;
+$row=$test->fetch_array();
+$_SESSION['charismaraw']=intval($row[0]);
+$_SESSION['strengthraw']=intval($row[1]);
+$_SESSION['intelraw']=intval($row[2]);
+$_SESSION['constitutionraw']=intval($row[3]);
+$_SESSION['wisdomraw']=intval($row[4]);
+$_SESSION['dexterityraw']=intval($row[5]);
 
 ?>
 

f08a6ece4616fe62e4261fa01385ff7c7f6178f1
Alex Livings <alivings@fingerlakes.edu>
2018-02-24 19:23:36 -0500

    the set values should now load into the basic.php page

--- basic.php
+++ basic.php
@@ -12,35 +12,35 @@ require_once "../config.php";
 			</div>
 			<div class="info">
 				<label>Race</label>
-				<input type="text" name="charrace" id="charrace" value="<?php echo $charrace; ?>">
+				<input type="text" name="charrace" id="charrace" value="<?php echo $_SESSION['charrace']; ?>">
 			</div>
 			<div class="info">
 				<label>Size</label>
-				<input type="text" name="charsize" id="charsize" value="<?php echo $charsize; ?>">
+				<input type="text" name="charsize" id="charsize" value="<?php echo $_SESSION['charsize']; ?>">
 			</div>
 			<div class="info">
 				<label>Alignment</label>
-				<input type="text" name="charalign" id="charalign" value="<?php echo $charalign; ?>">
+				<input type="text" name="charalign" id="charalign" value="<?php echo $_SESSION['charalign']; ?>">
 			</div>
 			<div class="info">
 				<label>Gender</label>
-				<input type="text" name="chargender" id="chargender" value="<?php echo $chargender; ?>">
+				<input type="text" name="chargender" id="chargender" value="<?php echo $_SESSION['chargender']; ?>">
 			</div>
 			<div class="info">
 				<label>Age</label>
-				<input type="text" name="charage" id="charage" value="<?php echo $charage; ?>">
+				<input type="text" name="charage" id="charage" value="<?php echo $_SESSION['charage']; ?>">
 			</div>
 			<div class="info">
 				<label>Height</label>
-				<input type="text" name="charheight" id="charheight" value="<?php echo $charheight; ?>">
+				<input type="text" name="charheight" id="charheight" value="<?php echo $_SESSION['charheight']; ?>">
 			</div>
 			<div class="info">
 				<label>Hair Colour</label>
-				<input type="text" name="charhair" id="charhair" value="<?php echo $charhair; ?>">
+				<input type="text" name="charhair" id="charhair" value="<?php echo $_SESSION['charhair']; ?>">
 			</div>
 			<div class="info">
 				<label>Eye Colour: </label>
-				<input type="text" name="chareyes" id="chareyes" value="<?php echo $chareyes; ?>">
+				<input type="text" name="chareyes" id="chareyes" value="<?php echo $_SESSION['chareyes']; ?>">
 			</div>
 
 			</div><input type="submit" name="save" class="btn"></div>

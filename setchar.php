3f20cec7987d07b5c1f0110bdf4e981fc6c4915a
Alex Livings <alivings@fingerlakes.edu>
2018-02-24 19:44:38 -0500

    if this doesn't break, it pulls all char info instead of just base stats

--- setchar.php
+++ setchar.php
@@ -8,7 +8,9 @@ $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 if(!empty($_GET)){
 $_SESSION['character'] = $_GET['charnames'];
-$get_character_info = "SELECT charisma_raw, strength_raw, intel_raw, constitution_raw, wisdom_raw, dexterity_raw FROM characters WHERE username='{$_SESSION['username']}' AND char_name='{$_SESSION['character']}'";
+$get_character_info = "SELECT charisma_raw, strength_raw, intel_raw, constitution_raw, wisdom_raw, dexterity_raw,
+                        char_level, char_race, char_size, char_alignment, char_gender, char_age, char_height, 
+                        char_hair, char_eye FROM characters WHERE username='{$_SESSION['username']}' AND char_name='{$_SESSION['character']}'";
 $test = mysqli_query($db, $get_character_info);
 $row=$test->fetch_array();
 $_SESSION['charismaraw']=intval($row[0]);
@@ -17,6 +19,16 @@ $_SESSION['intelraw']=intval($row[2]);
 $_SESSION['constitutionraw']=intval($row[3]);
 $_SESSION['wisdomraw']=intval($row[4]);
 $_SESSION['dexterityraw']=intval($row[5]);
+$_SESSION['charlvl']=intval($row[6]);
+$_SESSION['charrace']=intval($row[7]);
+$_SESSION['charsize']=intval($row[8]);
+$_SESSION['charalign']=intval($row[9]);
+$_SESSION['chargender']=intval($row[10]);
+$_SESSION['charage']=intval($row[11]);
+$_SESSION['charheight']=intval($row[12]);
+$_SESSION['charhair']=intval($row[13]);
+$_SESSION['chareyes']=intval($row[14]);
+
 }
 ?>
 
